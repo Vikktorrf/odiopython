8db9da216ab4ab1de639d9fe73de961082fc5628
Viktor_RF <120285458+Vikktorrf@users.noreply.github.com>
2025-03-19 08:51:25 -0600

    Update listadelistas.py

--- listadelistas.py
+++ listadelistas.py
@@ -1,4 +1,5 @@
 ##esta es la seccion que llena el diccionario
+"""
 vendedores= {}
 while True:
     nombre= input("dame elnombre del vendedor (escribe salir para terminar): ")
@@ -16,4 +17,22 @@ mv = int(input("dame una cantidad de marca de venta: "))
 
 ##muestra todos los datos de los vendedores
 lista = [[nombre,ventas] for nombre,ventas in vendedores.items() if (mv<ventas)]
+print(lista)
+"""
+
+
+##este es el codigo bueno
+vendedores = {}
+while True:
+    nombre = input("Dame el nombre del vendedor (escribe 'salir' para terminar): ")
+    if nombre.lower() == 'salir':
+        break
+    ventas = int(input("Ingresa los datos de las ventas: "))
+    vendedores[nombre] = ventas  # Simplifiqué esto, no es necesario usar una tupla para un solo valor
+
+# Segunda sección del código
+mv = int(input("Dame una cantidad de marca de venta: "))
+
+# Muestra solo los nombres de los vendedores cuyas ventas superan mv
+lista = [nombre for nombre, ventas in vendedores.items() if ventas > mv]
 print(lista)
